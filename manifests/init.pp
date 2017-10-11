6e164ac03b901ba300a99b832a9cf8ecb78cac03
root <root@aravindmaster.puppet.com>
2017-10-11 15:28:36 +0530

    with install and service manifest with params

--- manifests/init.pp
+++ manifests/init.pp
@@ -42,7 +42,12 @@
 #
 # Copyright 2017 Your name here, unless otherwise noted.
 #
-class ssh {
+class ssh (
+  String $package_name = $::ssh::params::package_name,
+  String $service_name = $::ssh::params::service_name,
+
+) inherits ::ssh::params {
+
   class { 'ssh::install': } ->         ###First install and check if service is running
   class { 'ssh::service': }
 
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,7 +1,11 @@
-class ssh::service {
+class ssh::service (
+  String $service_name = $::ssh::service_name,
+) {
+  
 # Service Resource to Ensure service is running
- service { 'ssh':
+ service { 'ssh-service':
    ensure   => running,
+   name     => $service_name,
    enable   => true,
      }
 }
